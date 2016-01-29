16a3df19b8caf788b3fa8a039737920634c4e157
Joe Hitchens <joe@sleepless.com>
2016-01-29 15:31:29 -0800

    -

--- evit.js
+++ evit.js
@@ -1,17 +1,22 @@
 
 
-var Evit = function() {
-	var self = this;
+var Evit = function(subj) {
+	var self = subj ? subj : this;
 
 	var listeners = {};
 
 	var emit = self.emit = function(ev) {
+		var args = Array.prototype.slice.call(arguments);
+		args.shift();		// drop event name
 		var a = listeners[ev];
 		if(a !== undefined) {
-			a.forEach(function(cb) {
-				cb(ev);
+			a.forEach(function(o) {
+				var cb = o.cb;
+				var t = o.t;
+				cb.apply(t, args);
 			});
 		}
+		return self;
 	}
 
 	var on = self.on = function(ev, cb) {
@@ -20,9 +25,11 @@ var Evit = function() {
 			a = [];
 			listeners[ev] = a;
 		}
-		a.push(cb);
+		a.push({t: this, cb:cb});
+		return self;
 	}
 
+	return self;
 };
 
 
--- test.js
+++ test.js
@@ -9,10 +9,18 @@ Thing.prototype = new Evit();
 
 t = new Thing();
 t.on("foo", function() {
-	console.log("foo happens");
+	console.log(this.name + ": foo happened");
 });
 t.emit("foo");			// prints "foo happens"
 t.emit("bar");			// 
+
+
+Evit({ name: "Elmer", })
+.on("ouch", function(utterance) {
+	console.log(this.name+" says, "+utterance);
+})
+.emit("ouch", "Aaargh!!");
+
 
 
 
